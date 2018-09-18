c15ea00e274e2f9a0d917b22b5e2e6669e61defe
finale100 <finale150@gmail.com>
2018-09-18 16:59:46 -0400

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
-class UserInput extends Component {
 
+class UserInput extends Component {
   state = {
     username: '',
     hometown: ''
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux'
 class Users extends Component {
 
   render() {
+
     return (
       <div>
         <ul>
-          Users!
+          {this.props.users.map(user => {
+            return <li>{user.username}</li>
+          })}
         </ul>
+        {this.props.users.length}
       </div>
     )
   }
 }
 
-//add mapStateToProps here
-
-export default Users
+const mapStateToProps = (state) => {
+  return { users: state.users }
+}
+export default connect(mapStateToProps)(Users)
